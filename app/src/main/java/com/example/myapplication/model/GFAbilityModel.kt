fa29f2da4ce2d9af9ab3948a735ee36612d34a02
VlSpCh <69346152+VlSpCh@users.noreply.github.com>
2023-03-26 17:29:53 +0400

    Добавил GFAbilityModel.kt и GFAbilityTypes.kt; обновил GFModel.kt и GfRepository.kt

---
+++ app/src/main/java/com/example/myapplication/model/GFAbilityModel.kt
@@ -0,0 +1,6 @@
+package com.example.myapplication.model
+
+data class GFAbility(
+    val name: String,
+    val type: GFAbilityType
+)
---
+++ app/src/main/java/com/example/myapplication/model/GFAbilityTypes.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication.model
+
+enum class GFAbilityType {
+    ABILITY_JUNCTION,
+    ABILITY_COMMAND,
+    ABILITY_CHARACTER,
+    ABILITY_PARTY,
+    ABILITY_GF,
+    ABILITY_MENU
+}

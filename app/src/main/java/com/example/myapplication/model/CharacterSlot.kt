ec99276dc4ba3fe26b998544dadbbff00ce3a5b6
screloma <screloma@gmail.com>
2023-03-26 12:07:15 +0300

    Убрал лишний параметр из модели слота персонажа

--- app/src/main/java/com/example/myapplication/model/CharacterSlot.kt
+++ app/src/main/java/com/example/myapplication/model/CharacterSlot.kt
@@ -3,6 +3,5 @@ package com.example.myapplication.model
 data class CharacterSlot(
     val id: String,
     var selectedGF: GFModel? = null,
-    var selectedGfTag: String? = null,
     var selectedSkills: MutableList<String> = mutableListOf()
 )
